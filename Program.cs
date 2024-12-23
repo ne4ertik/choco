3db2fe8609f030e5b994a210cdfae9443b283d37
Anya ฅ^•ﻌ•^ฅ <147582797+ne4ertik@users.noreply.github.com>
2024-12-23 11:03:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int money = Convert.ToInt32(Console.ReadLine());
+            int price = Convert.ToInt32(Console.ReadLine());
+            int wrap = Convert.ToInt32(Console.ReadLine());
+
+            int vsegochocolate = Chocolatemaximum(money, price, wrap);
+
+            Console.WriteLine($"Максимально возможное количество шоколадок: {Chocolatemaximum}");
+        }
+
+        static int Chocolatemaximum(int money, int price, int wrap)
+        {
+
+            int chocolates = money / price;
+            int wrappers = chocolates;
+
+
+            while (wrappers >= wrap)
+            {
+
+                int newChocolates = wrappers / wrap;
+                chocolates += newChocolates;
+                wrappers = newChocolates + (wrappers % wrap);
+            }
+
+            return chocolates;
+        }
+    }
+    }
+}
